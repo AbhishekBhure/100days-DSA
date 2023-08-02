2ecaba43762eb8cf64bcb0cfaf831bda57887ecc
AbhishekBhure <abhiab7411@gmail.com>
2023-08-02 21:49:36 +0530

    changes in zigzag.js ZigZag Squence

--- js/zigzag.js
+++ js/zigzag.js
@@ -1,24 +1,24 @@
 function zigzag(a) {
-  //sort the aay first
-  //   a.sort((a, b) => a - b);
-
-  //swaping adjecent to get zig zag pattern
-  //   for (let i = 1; i < a.length - 1; i += 2) {
-  // const temp = a[i];
-  // a[i] = a[i + 1];
-  // a[i + 1] = temp;
-  // [a[i], a[i + 1]] = [a[i + 1], a[i]];
-  //   }
-
+  //   sort the aay first
   a.sort((a, b) => a - b);
-  let n = a.length;
-  let mid = Math.floor((n - 1) / 2);
+
+  //   swaping adjecent to get zig zag pattern
+  for (let i = 1; i < a.length - 1; i += 2) {
+    const temp = a[i];
+    a[i] = a[i + 1];
+    a[i + 1] = temp;
+    [a[i], a[i + 1]] = [a[i + 1], a[i]];
+  }
+
+  //   a.sort((a, b) => a - b);
+  //   let n = a.length;
+  //   let mid = Math.floor((n - 1) / 2);
 
   // Swap elements from mid to n-2 with elements from n-1 to mid+1
-  for (let i = mid; i < n - 1; i++) {
-    [a[i], a[n - 1]] = [a[n - 1], a[i]];
-    n--;
-  }
+  //   for (let i = mid; i < n - 1; i++) {
+  //     [a[i], a[n - 1]] = [a[n - 1], a[i]];
+  //     n--;
+  //   }
 
   return a;
 }
