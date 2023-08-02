73f91b650ee5bc0aabbc111b99d8f30c451607bd
AbhishekBhure <abhiab7411@gmail.com>
2023-08-02 20:35:59 +0530

    changes in timeConversion from 24 hours to 12 hours format

--- hackerRank/timeConversion.js
+++ hackerRank/timeConversion.js
@@ -1,3 +1,4 @@
+// time conversion from 12 to 24 hours format
 function timeConversion(s) {
   // Write your code here
 
@@ -20,5 +21,26 @@ function timeConversion(s) {
     .padStart(2, "0")}:${seconds.toString()} `;
 }
 
-console.log(timeConversion("07:05:45PM"));
+// console.log(timeConversion("07:05:45PM"));
 // console.log(timeConversion("07:05:45 PM"));
+
+function timeConversion24to12(t) {
+  let [hour, minute, ...rest] = t.split(":");
+  let seconds = rest.toString().slice(0, 2);
+  let period = rest.toString().slice(2);
+
+  hour = parseInt(hour);
+  minute = parseInt(minute);
+
+  if (hour === 0) {
+    hour = 0;
+  } else if (hour > 12) {
+    hour -= 12;
+  }
+
+  return `${hour.toString().padStart(2, "0")}:${minute
+    .toString()
+    .padStart(2, "0")}:${seconds.toString()} ${period}`;
+}
+
+console.log(timeConversion24to12("19:05:45PM"));
