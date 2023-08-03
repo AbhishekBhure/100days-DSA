96fab398b4fe3ab4a14b4920a6b1d9960b33f839
AbhishekBhure <abhiab7411@gmail.com>
2023-08-03 11:31:58 +0530

    anagramCheck.js

---
+++ js/anagramCheck.js
@@ -0,0 +1,8 @@
+function anagramCheck(str1, str2) {
+  const string1 = str1.split("").sort().join("");
+  const string2 = str2.split("").sort().join("");
+
+  return string1 === string2;
+}
+
+console.log(anagramCheck("cat", "car"));
