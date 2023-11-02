e60cc29f1b2706c886c2f4d0454f20d18599197b
AbhishekBhure <abhiab7411@gmail.com>
2023-11-02 23:12:44 +0530

    duplicate.js with filter method

--- js/duplicate.js
+++ js/duplicate.js
@@ -4,15 +4,19 @@ const arr1 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 
 const removeDup = [];
 
-for (let i = 0; i < arr1.length; i++) {
-  if (i < arr1.length - 1) {
-    if (arr1[i] !== arr1[i + 1]) {
-      removeDup.push(arr1[i]);
-    }
-  } else {
-    removeDup.push(arr1[i]);
-  }
-}
+// for (let i = 0; i < arr1.length; i++) {
+//   if (i < arr1.length - 1) {
+//     if (arr1[i] !== arr1[i + 1]) {
+//       removeDup.push(arr1[i]);
+//     }
+//   } else {
+//     removeDup.push(arr1[i]);
+//   }
+// }
 
-console.log(arr1);
+let res = arr1.filter((val, index, self) => {
+  return self.indexOf(val) === index;
+});
+
+console.log(res);
 console.log(removeDup);
