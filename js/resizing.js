bc95809148a83ded24ebdd3d394c30e2bc40115f
AbhishekBhure <abhiab7411@gmail.com>
2023-11-03 22:21:05 +0530

    resizing.js

---
+++ js/resizing.js
@@ -0,0 +1,18 @@
+arr1 = [1, 2, 3, 4, 5];
+val = 2;
+count = 0;
+var reduceLength = false;
+
+for (let i = 0; i < arr1.length; i++) {
+  if (arr1[i] != val) {
+    arr1[count++] = arr1[i];
+  } else {
+    reduceLength = true;
+  }
+}
+
+if (reduceLength) {
+  arr1.length -= 1;
+}
+
+console.log(arr1);
