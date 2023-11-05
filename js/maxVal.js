434a75d5474fa829e337a9faad2976cf2ace59f9
AbhishekBhure <abhiab7411@gmail.com>
2023-11-05 23:02:23 +0530

    maxVal.js using Math.max()

--- js/maxVal.js
+++ js/maxVal.js
@@ -1,10 +1,12 @@
 const arr = [1, 8, 9, 6, 5, 45, 76];
-var maxVal = arr[0];
 
-for (let i = 0; i < arr.length; i++) {
-  if (maxVal < arr[i]) {
-    maxVal = arr[i];
-  }
-}
+console.log(Math.max(...arr));
+// var maxVal = arr[0];
 
-console.log(maxVal);
+// for (let i = 0; i < arr.length; i++) {
+//   if (maxVal < arr[i]) {
+//     maxVal = arr[i];
+//   }
+// }
+
+// console.log(maxVal);
