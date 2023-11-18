e264c0daf298a2e8c67dfacb8ed95eb5ad0215aa
AbhishekBhure <abhiab7411@gmail.com>
2023-11-18 17:40:19 +0530

    adding bubbleSort.js

---
+++ js/sort/bubbleSort.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+  let n = arr.length;
+  var swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < n - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+    n--;
+  } while (swapped);
+  return arr;
+}
+
+console.log(bubbleSort([4, 7, 2, 1, 9]));
