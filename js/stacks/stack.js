c3c09cdd240e53783be1eb54c4463a1e91be5e84
AbhishekBhure <abhiab7411@gmail.com>
2023-11-19 23:21:09 +0530

    working with stacks

---
+++ js/stacks/stack.js
@@ -0,0 +1,49 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(item) {
+    this.items.push(item);
+  }
+
+  pop() {
+    if (this.items.length === 0) {
+      return "UnderFlow";
+    }
+    return this.items.pop();
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  top() {
+    if (this.items.length === 0) {
+      return "UnderFlow";
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  printStack() {
+    let str = "";
+    for (let i = 0; i < this.items.length; i++) {
+      str += this.items[i] + ",";
+    }
+
+    console.log(str);
+  }
+}
+
+let stack = new Stack();
+
+console.log(stack.isEmpty());
+stack.push(10);
+stack.push(20);
+stack.push(30);
+stack.push(40);
+stack.push(50);
+stack.pop();
+stack.printStack();
+console.log(stack.isEmpty());
+console.log(stack.top());
