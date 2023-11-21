267540b2a572271ea8c283aab22f0d8fc0cf64f3
AbhishekBhure <abhiab7411@gmail.com>
2023-11-22 00:29:21 +0530

    twoSum.js

--- js/twoSum.js
+++ js/twoSum.js
@@ -1,14 +1,22 @@
-var twoSum = function (nums, target) {
-  const obj = {};
+// var twoSum = function (nums, target) {
+//   const obj = {};
 
+//   for (let i = 0; i < nums.length; i++) {
+//     const res = target - nums[i];
+//     if (Object.keys(obj).includes(String(nums[i]))) {
+//       return [i, obj[nums[i]]];
+//     } else {
+//       obj[res] = i;
+//     }
+//   }
+// };
+
+var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
-    const res = target - nums[i];
-    if (Object.keys(obj).includes(String(nums[i]))) {
-      return [i, obj[nums[i]]];
-    } else {
-      obj[res] = i;
+    for (j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) return [i, j];
     }
   }
 };
 
-console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 2, 3], 6));
