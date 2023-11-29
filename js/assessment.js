534e2f649953ef7ae09f471e9304fcee67de3591
AbhishekBhure <abhiab7411@gmail.com>
2023-11-29 23:18:41 +0530

    assessment.js

---
+++ js/assessment.js
@@ -0,0 +1,27 @@
+// class database {
+//   constructor() {
+//     this.connection = "hi";
+//   }
+// }
+
+// class Order extends database {
+//   constructor() {
+//     super();
+//   }
+
+//   sendOrder() {
+//     connection.sendOrder();
+//   }
+// }
+
+// let order = new Order();
+// console.log(order.connection);
+
+// const nums = [2, 5, 7];
+// nums.length = 4;
+// console.log(nums);
+
+let a = -16;
+let b = 5;
+
+console.log(a % b);
