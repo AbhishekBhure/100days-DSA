c7a1fe4b0e845cf98b5c3132bf3fa433df9772c5
AbhishekBhure <abhiab7411@gmail.com>
2023-11-30 23:38:46 +0530

    working with strings

---
+++ js/strings/romanToInt.js
@@ -0,0 +1,20 @@
+symbols = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+function romanToInt(s) {
+  value = 0;
+  for (let i = 0; i < s.length - 1; i += 1) {
+    symbols[s[i]] < symbols[s[i + 1]]
+      ? (value -= symbols[s[i]])
+      : (value += symbols[s[i]]);
+  }
+  return value + symbols[s[s.length - 1]];
+}
+
+console.log(romanToInt("III"));
