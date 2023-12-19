f965b0f408dadf8f6f2c5ed31706d1f1fde62309
AbhishekBhure <abhiab7411@gmail.com>
2023-12-19 12:03:24 +0530

    modified twoSum.js using two poitners

--- js/twoSum.js
+++ js/twoSum.js
@@ -1,3 +1,4 @@
+// O(n);
 // var twoSum = function (nums, target) {
 //   const obj = {};
 
@@ -11,12 +12,37 @@
 //   }
 // };
 
-var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) return [i, j];
+//Brute force approach
+// O(n^2)
+// var twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) return [i, j];
+//     }
+//   }
+// };
+
+//two pointers
+function twoSum(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+
+    if (sum > target) {
+      right--;
+    } else if (sum < target) {
+      left++;
+    } else {
+      return [left, right];
     }
   }
-};
+  return [-1, -1];
 
-console.log(twoSum([3, 2, 3], 6));
+  console.log("left", left);
+  console.log("right", right);
+}
+
+console.log(twoSum([1, 2, 3, 8, 9, 20], 12));
+// console.log(twoSum([3, 2, 3], 6));
