94fe3d32241ead3cf53ee774b1a020ce5e86724c
AbhishekBhure <abhiab7411@gmail.com>
2023-12-19 13:15:08 +0530

    balancedOarenthesis.js modified

--- js/stacks/balancedParenthesis.js
+++ js/stacks/balancedParenthesis.js
@@ -3,22 +3,51 @@ const Stack = require("./stack");
 // let str1 = "()()";
 let str2 = "()(()()";
 
-function balancedParenthesis(str1) {
-  let stack = new Stack();
+// function balancedParenthesis(str1) {
+//   let stack = new Stack();
 
-  let arr = str1.split(""); //can also slove this with for of loop where we do not need to split the str
+//   let arr = str1.split(""); //can also slove this with for of loop where we do not need to split the str
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "(" || arr[i] === "{" || arr[i] === "[") {
-      stack.push(arr[i]);
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === "(" || arr[i] === "{" || arr[i] === "[") {
+//       stack.push(arr[i]);
+//     } else {
+//       if (stack.isEmpty()) {
+//         return false;
+//       }
+//       stack.pop();
+//     }
+//   }
+//   return stack.isEmpty();
+// }
+
+//the ans we are getting is
+// (){{[[]]}} => true
+// ([)] => true this ans is wrong so our code is also wrong
+
+function balancedParenthesis(str) {
+  let stack = [];
+  const pairs = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  for (let char of str) {
+    if (pairs[char] !== undefined) {
+      stack.push(char);
     } else {
-      if (stack.isEmpty()) {
+      let top = stack.pop();
+      if (pairs[top] !== char) {
         return false;
       }
-      stack.pop();
     }
   }
-  return stack.isEmpty();
+  return stack.length === 0;
 }
 
-console.log(balancedParenthesis("(){{[[]]}}"));
+let parenthesis = ["(){{[[]]}}", "([)]"];
+
+parenthesis.forEach((parenthesis) => {
+  console.log(parenthesis, balancedParenthesis(parenthesis));
+});
